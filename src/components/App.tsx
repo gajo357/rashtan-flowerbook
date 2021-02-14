2f6d7ece3c991bfbf1c0e66bd1a97fcce026a813
Gavrilo Mumovic <gajo357@gmail.com>
2021-02-14 14:34:34 +0100

    Landing page and font

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,75 +1,77 @@
 import React, { useState } from "react";
-import logo from "../assets/logo.svg";
+import Logo from "../assets/logo.svg?component";
 import Flower from "../assets/flower.svg?component";
+import Bouquet from "../assets/bouquet.svg?component";
 import Trash from "../assets/trash.svg?component";
 import Dollars from "../assets/dollars.svg?component";
 import New from "../assets/new.svg?component";
 import "../theme/App.less";
 import AppButton from "./AppButton";
-import { Col, Row } from "antd";
+import { Col, Row, Layout, Space } from "antd";
+import Icon, { MenuOutlined } from "@ant-design/icons";
+
+const { Header, Content } = Layout;
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Layout>
+        <Header>
+          <Row>
+            <Col flex="1">
+              <Icon component={Flower} className="icon-logo" />
+            </Col>
+            <Col flex="4">
+              <p>FlowersBook</p>
+            </Col>
+            <Col flex="1">
+              <MenuOutlined />
+            </Col>
+          </Row>
+        </Header>
+        <Content>
+          <Icon component={Logo} className="App-logo" />
 
-        <Row gutter={[16, 16]}>
-          <Col flex={1}>
-            <AppButton
-              onClick={() => setCount(count => count + 1)}
-              icon={Flower}
-            >
-              Utrošeno
-            </AppButton>
-          </Col>
-          <Col flex={1}>
-            <AppButton
-              onClick={() => setCount(count => count + 1)}
-              icon={Trash}
-            >
-              Bačeno
-            </AppButton>
-          </Col>
-        </Row>
-        <Row gutter={[16, 16]} style={{ marginTop: "0.5em" }}>
-          <Col flex={1}>
-            <AppButton
-              onClick={() => setCount(count => count + 1)}
-              icon={Dollars}
-            >
-              Pazar
-            </AppButton>
-          </Col>
-          <Col flex={1}>
-            <AppButton onClick={() => setCount(count => count + 1)} icon={New}>
-              Novo
-            </AppButton>
-          </Col>
-        </Row>
-
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+          <Row gutter={16}>
+            <Col flex={1}>
+              <AppButton
+                onClick={() => setCount(count => count + 1)}
+                icon={Bouquet}
+              >
+                Prodato
+              </AppButton>
+            </Col>
+            <Col flex={1}>
+              <AppButton
+                onClick={() => setCount(count => count + 1)}
+                icon={Trash}
+              >
+                Bačeno
+              </AppButton>
+            </Col>
+          </Row>
+          <Row gutter={16} style={{ marginTop: "1em" }}>
+            <Col flex={1}>
+              <AppButton
+                onClick={() => setCount(count => count + 1)}
+                icon={Dollars}
+              >
+                Pazar
+              </AppButton>
+            </Col>
+            <Col flex={1}>
+              <AppButton
+                onClick={() => setCount(count => count + 1)}
+                icon={New}
+              >
+                Novo
+              </AppButton>
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
     </div>
   );
 };
