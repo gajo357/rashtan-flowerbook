e05387f984eeb5276701e928e7f2bc2a56318869
Gavrilo Mumovic <gajo357@gmail.com>
2021-07-06 12:12:49 +0200

    Updated yarn version

--- src/hooks/Constants.ts
+++ src/hooks/Constants.ts
@@ -1,6 +1,6 @@
 // const baseUrl = "https://rashtan-flowerbook-api.azurewebsites.net/api/";
 const appUrl = "https://rashtan-flowerbook.azurewebsites.net/";
-const baseUrl = "https://localhost:44309/api/";
+const baseUrl = process.env.REACT_APP_API_URL ?? "https://localhost:44309/api/";
 // const appUrl = "http://localhost:8100/";
 
 export { baseUrl, appUrl };
--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,8 @@ export default defineConfig({
   },
   build: {
     outDir: "build"
+  },
+  define: {
+    "process.env": {}
   }
 });
